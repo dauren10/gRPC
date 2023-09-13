516ebae58e67c829fe7e1ceb9f4676d2dc9203bf
Dauren Shalabayev <dauren.shalabayev@gmail.com>
2023-09-13 12:57:56 +0600

    Unary

--- server/unary.go
+++ server/unary.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"context"
+
+	pb "github.com/akhil/grpc-demo-yt/proto"
+)
+
+func (s *helloServer) SayHello(ctx context.Context, req *pb.NoParam) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{
+		Message: "Hello",
+	}, nil
+}
